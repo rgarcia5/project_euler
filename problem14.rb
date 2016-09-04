1a5915f7e694c26c2bcfa088251c4e6bcab4a8b6
Raymond Garcia <raymond_jay_garcia@yahoo.com>
2016-09-03 23:16:12 -0700

    Update problem14.rb

--- problem14.rb
+++ problem14.rb
@@ -3,16 +3,14 @@ def collatz_sequence(num)
   count = 0
   until result == 1
     if result % 2 == 0
-  	  result = result/2
+      result = result/2
     else
-  	  result = (3*result + 1)
+      result = (3*result + 1)
     end
     count += 1
   end
 
-  
   count + 1
-  
 end
 
 
@@ -21,15 +19,12 @@ def iterate
   i = (1_000_000)
   while i > 1
    current_count = collatz_sequence(i)
-  
    if current_count > greatest_count[1]
    	 greatest_count[0] = i
      greatest_count[1] = current_count
    end
-   
   
    i -= 1
-  
   end
 
   greatest_count[0]
