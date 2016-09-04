f8d51f2c38097153829b04e95c9fef9dc7dfe8d9
Raymond Garcia <raymond_jay_garcia@yahoo.com>
2016-09-03 23:06:06 -0700

    Update problem1.rb

--- problem1.rb
+++ problem1.rb
@@ -2,9 +2,9 @@ def sum_of_multiples
   i = 1
   ary = []
   while i < 1000
-  	if i % 3 == 0 || i % 5 == 0
-  		ary << i
-  	end
+    if i % 3 == 0 || i % 5 == 0
+  	  ary << i
+    end
   	i += 1
   end
   
